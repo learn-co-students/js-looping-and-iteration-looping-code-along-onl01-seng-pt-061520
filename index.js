d9d70a74665ff8105d1b0ecd03f86ab438609b41
miloscomplex <hicallmesutton@gmail.com>
2020-12-14 23:24:48 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+  let thankYouMessages = []
+  for (var i = 0; i < array.length; i++) {
+    thankYouMessages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return thankYouMessages;
+}
+
+function countDown(int) {
+  while (int >= 0) {
+    console.log(int);
+    int--;
+  }
+}
