1bb79d7ee3c63ad449ee870f2e7e7cb3ad8a84e2
github username <charliepbarth@gmail.com>
2020-12-16 21:38:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let temp = []
+    for (let i = 0; i < array.length; i++) {
+        temp.push( `Thank you, ${array[i]}, for the wonderful ${event} gift!` )
+    }
+    return temp
+}
+  
+function countDown(num) {
+    while (num > -1) {
+      console.log(num)
+      num = num - 1
+    }
+}
