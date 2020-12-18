a63f18a88ccbf9c3a3cb22c427b4717267d53b5c
Jessie <jssdnvn.com>
2020-12-18 11:10:27 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(name) {
+   let thankYou = []
+   for (let i = 0; i < name.length; i++) {
+      thankYou.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)  
+   }
+   return thankYou
+}
+
+let number = 10 
+
+function countDown(number) {
+
+   while (number > -1 ) {
+      console.log(number);
+      number -= 1 ;
+   }
+
+}
