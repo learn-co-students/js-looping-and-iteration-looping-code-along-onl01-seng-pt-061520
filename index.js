8a91445b9bb16197a657db532a3ed46105284f4e
Lukasz Kowalik <kowalix007@gmail.com>
2020-12-28 22:23:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+const eventName = ("birthday");
+const cardsarray = [];
+
+function writeCards(names,eventName){
+  for (let i = 0; i < names.length; i++) {
+  cardsarray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+}
+return cardsarray
+}
+
+function countDown(num){
+  while (num >= 0) {
+    console.log(num--);
+  }
+}
