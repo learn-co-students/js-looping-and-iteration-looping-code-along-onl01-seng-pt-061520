371d561406cc12b7b2e014b1f3a8d81176c8ab64
jbmi2000 <jeff.marbury@gmail.com>
2021-01-02 09:52:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+
+const names = [ 'Lisa', 'Kaitin', 'Jan'];
+const events = ['surprise'];
+function writeCards(names, events){
+    for (let i=0; i < names.length; i++){
+        console.log(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+    //return names;
+}
+
+writeCards(names,events);
